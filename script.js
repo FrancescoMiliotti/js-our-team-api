af2aa46beb2319a9b0b6c66a841fcf56992f8dcd
francesco.miliotti <francesco.miliotti@email.com>
2025-11-25 17:26:48 +0100

    eseguo la chiamata e stampo in pagina i dati

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
-const teamMembers = [
-  {
-    name: "Marco Bianchi",
-    role: "Designer",
-    email: "marcobianchi@team.com",
-    img: "img/male1.png"
-  },
-  {
-    name: "Laura Rossi",
-    role: "Front-end Developer",
-    email: "laurarossi@team.com",
-    img: "img/female1.png"
-  },
-  {
-    name: "Giorgio Verdi",
-    role: "Back-end Developer",
-    email: "giorgioverdi@team.com",
-    img: "img/male2.png"
-  },
-  {
-    name: "Marta Ipsum",
-    role: "SEO Specialist",
-    email: "martarossi@team.com",
-    img: "img/female2.png"
-  },
-  {
-    name: "Roberto Lorem",
-    role: "SEO Specialist",
-    email: "robertolorem@team.com",
-    img: "img/male3.png"
-  },
-  {
-    name: "Daniela Amet",
-    role: "Analyst",
-    email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
-];
 
 
 
+// faccio la chiamata Api e stampo in console il risultato dell'oggetto Data
 axios.get("https://boolean-teachers.github.io/mock/api/members/").then(risposta => {
 
   const rispostaDelServer = risposta.data;
@@ -47,7 +10,7 @@ axios.get("https://boolean-teachers.github.io/mock/api/members/").then(risposta 
 
 
 
-
+// Recupero gli id html da modificare
 
 document.getElementById("button").addEventListener("click", function () {
 
@@ -56,6 +19,8 @@ document.getElementById("button").addEventListener("click", function () {
 
 const teamMembersEl = document.getElementById("card-member");
 const memberCard = teamMembersEl
+
+// effetuo il ciclo sulla risposta del server e stampo in pagina i dati recuperati
 
 for (let i = 0; i < rispostaDelServer.length; i++) {
   let member = rispostaDelServer[i];
@@ -82,6 +47,9 @@ for (let i = 0; i < rispostaDelServer.length; i++) {
 
 
 }
+
+
+
 console.log(memberCard);
 })
 
